b99b2598b961cd3bd93f54175df4625e575c13f3
IHSAN ADI PRATAMA <ihsan.pratama@bankmandiri.co.id>
2023-05-01 08:52:28 +0700

    generate seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin Role',
+            'email' => 'admin@testrole.com',
+            'password' => bcrypt('admin123')
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'User Role',
+            'email' => 'user@testrole.com',
+            'password' => bcrypt('user123')
+        ]);
+
+        $user->assignRole('user');
+
+    }
+}
